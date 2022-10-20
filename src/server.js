5c775912e464f6548f56f6897ad95d66df9b9caf
Onai Nadapdap <onainadapdap1@gmail.com>
2022-10-20 09:50:43 +0700

    install package dotenv, konfigurasi environment di .env dan server.js, membuat file migration dan membuat table songs dan albums

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 /*
     *Server konfiguration
 */
+// import  dotenv dan menjalankan konfigurasinya
+require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
     // HTTP server sendiri dibuat melalui method Hapi.server(), menerima 1 parameter ServerOptions
     // ServerOptions merupakan objek yang menampung konfigurasi dari server yang hendak dibuat
     const server = Hapi.server({
-        port: 5000,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        // Sekarang berkas .env sudah dibaca oleh Node.js dan kini nilai dari variable 
+        // environment yang ada di berkas .env dapat diakses melalui properti process.env.
+        port: process.env.PORT,
+        host: process.env.HOST,
         routes: {
             cors: {
                 origin: ['*'],
