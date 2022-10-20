052ae9da3d6a821e580f46e18c5a7a1d28ff96d7
Onai Nadapdap <onainadapdap1@gmail.com>
2022-10-20 08:46:03 +0700

    Konfigurasi server.js dan menambahkan struktur folder plugin albums dan songs

--- src/server.js
+++ src/server.js
@@ -1,3 +1,23 @@
 /*
     *Server konfiguration
-*/ 
+*/
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+    // HTTP server sendiri dibuat melalui method Hapi.server(), menerima 1 parameter ServerOptions
+    // ServerOptions merupakan objek yang menampung konfigurasi dari server yang hendak dibuat
+    const server = Hapi.server({
+        port: 5000,
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+}
+
+init();
